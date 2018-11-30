088cf5f20efc5317113ff545f822abf90de1cc0f
jinyanhuan <jinyanhuan@gmail.com>
2018-11-30 10:06:23 +0800

    moderate

--- pie_graph.py
+++ pie_graph.py
@@ -1,16 +1,17 @@
 import matplotlib.pyplot as plt
 
+Title = input("Please input the title: ")
 while True:
-    values = list(input('Please input the values:').split())
-    labels = list(input('Please input the labels:').split())
+    values = list(input('Please input the values: ').split())
+    labels = list(input('Please input the labels: ').split())
     if len(values) != len(labels):
         print('You values does not equal to your labels, please restart')
     else:
         break
-colors_list = ['y','g','r','c','m','b']
+colors_list = ['r','g','y','b','m','c']
 colors = colors_list[:len(labels)]
 plt.pie(values, colors=colors, labels=labels, autopct='%1.1f%%',
         counterclock=False, shadow=False)
-plt.title('Values')
+plt.title(Title)
 
 plt.show()
